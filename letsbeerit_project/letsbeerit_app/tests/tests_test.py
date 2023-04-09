39c46b1d4d7d55320292dfeab3751816d600ae03
pawewisn <pawel.wis.me@gmail.com>
2023-04-09 18:19:41 -0400

    first users tests

--- letsbeerit_project/letsbeerit_app/tests/tests_test.py
+++ letsbeerit_project/letsbeerit_app/tests/tests_test.py
@@ -7,3 +7,13 @@ class TestView(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Hello World')
+
+
+class TestUsersListView(TestCase):
+    def test_users_list_view(self):
+        response = self.client.get('/users')
+        self.assertEqual(response.status_code, 401)
+        self.assertContains(response, 'Authentication credentials were not provided')
+
+    def test_users_authentication(self):
+        pass
