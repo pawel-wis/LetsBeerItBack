d0d9ae79a35739b9b87d7713b38811c6c51d66cc
pawewisn <pawel.wis.me@gmail.com>
2023-04-09 18:19:48 -0400

    users url

--- letsbeerit_project/letsbeerit_app/urls.py
+++ letsbeerit_project/letsbeerit_app/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, include
-from .views import hello
+from django.urls import path
+from .views import hello, UsersListView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', hello),
+    path('users', UsersListView.as_view()),
+    path('users/get-token', obtain_auth_token, name='get-token'),
 ]
