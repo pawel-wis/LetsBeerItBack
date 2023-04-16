403662b907b84b23ff9b0e52d036741fdbb4c9c8
pawewisn <pawel.wis.me@gmail.com>
2023-04-16 17:37:42 -0400

    membership factory

--- letsbeerit_project/letsbeerit_app/tests/test_utils/factories.py
+++ letsbeerit_project/letsbeerit_app/tests/test_utils/factories.py
@@ -1,5 +1,5 @@
 from factory.django import DjangoModelFactory
-from ...models import AppUser, SocialGroup
+from ...models import AppUser, SocialGroup, SocialMembership
 from factory.faker import Faker
 
 
@@ -9,10 +9,22 @@ class AppUserFactory(DjangoModelFactory):
 
     username = Faker("user_name")
     password = Faker("password")
+    email = Faker("email")
 
 
-class SoliacGroupFactory(DjangoModelFactory):
+class SocialGroupFactory(DjangoModelFactory):
     class Meta:
         model = SocialGroup
 
     name = Faker("sentence", nb_words=3)
+    description = Faker("sentence", nb_words=10)
+
+
+class SocialMembershipFactory(DjangoModelFactory):
+    class Meta:
+        model = SocialMembership
+
+    user = AppUserFactory()
+    group = SocialGroupFactory()
+    appuser_group_role = Faker("random_element", elements=("member", "admin"))
+
