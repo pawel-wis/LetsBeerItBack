8533cc4329a2b4f7bbf5957cde4453840703c4af
pawewisn <pawel.wis.me@gmail.com>
2023-04-17 10:00:07 -0400

    add models to admin page

--- letsbeerit_project/letsbeerit_app/admin.py
+++ letsbeerit_project/letsbeerit_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import AppUser, SocialGroup
+from .models import AppUser, SocialGroup, SocialMembership
 
 admin.site.register(AppUser)
 admin.site.register(SocialGroup)
+admin.site.register(SocialMembership)
