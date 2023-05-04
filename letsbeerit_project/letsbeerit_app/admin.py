50a7497c3b8795a65249b368ee4460b828d9c158
pawewisn <pawel.wis.me@gmail.com>
2023-05-04 17:40:40 -0400

    add user pin to admin view

--- letsbeerit_project/letsbeerit_app/admin.py
+++ letsbeerit_project/letsbeerit_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import AppUser, SocialGroup, SocialMembership
+from .models import AppUser, SocialGroup, SocialMembership, UserPin
 
 admin.site.register(AppUser)
 admin.site.register(SocialGroup)
 admin.site.register(SocialMembership)
+admin.site.register(UserPin)
